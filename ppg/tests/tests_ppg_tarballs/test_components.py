7f1861eb54f191c073516970a694f6de75d29eb7
Shahid Ullah <shahidullah.khan@percona.com>
2024-05-30 03:07:56 +0500

    Define VERSION enviromnet variable to fix major version issue

--- ppg/tests/tests_ppg_tarballs/test_components.py
+++ ppg/tests/tests_ppg_tarballs/test_components.py
@@ -2,7 +2,8 @@ import os
 import pytest
 
 import testinfra.utils.ansible_runner
-
+SERVER_VERSION = os.getenv('server_version')
+VERSION = f'ppg-{SERVER_VERSION}'
 from .. import settings
 # from ppg.tests.settings import get_settings, MAJOR_VER
 
