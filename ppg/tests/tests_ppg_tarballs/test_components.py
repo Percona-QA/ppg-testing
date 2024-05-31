ed902e65f1bc7603c5aaae1ad871d8d56e47079d
Shahid Ullah <shahidullah.khan@percona.com>
2024-05-31 15:54:02 +0500

    Update create languages statements

--- ppg/tests/tests_ppg_tarballs/test_components.py
+++ ppg/tests/tests_ppg_tarballs/test_components.py
@@ -18,8 +18,12 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 os.environ['PATH'] = f"{PG_PATH}/bin:{INSTALL_PATH}/percona-pgbouncer/bin/:{INSTALL_PATH}/percona-haproxy/sbin:{INSTALL_PATH}/percona-patroni/bin:{INSTALL_PATH}/percona-pgbackrest/bin:{INSTALL_PATH}/percona-pgbadger:{INSTALL_PATH}/percona-pgpool-II/bin:" + os.environ['PATH']
 
-PACKAGES = ["libecpg-compat3",  "libecpg-dev", 'libecpg6', "libpgtypes3", "libpq-dev",  "libpq5"]
+PACKAGES = ["libecpg_compat.so.3", 'libecpg.so.6', "libpgtypes.so.3", "libpq.so.5"]
 pg_versions = settings.get_settings(os.environ['MOLECULE_SCENARIO_NAME'])[os.getenv("VERSION")]
+
+@pytest.fixture(scope='session')
+def get_server_path(scope='session'):
+    return PG_PATH
 
 @pytest.fixture(scope='session')
 def get_psql_binary_path(scope='session'):
@@ -29,7 +33,9 @@ def get_psql_binary_path(scope='session'):
 @pytest.fixture()
 def perl_function(host,get_psql_binary_path):
     with host.sudo("postgres"):
-        install_extension = host.run(f"PATH=/opt/percona-perl/bin:$PATH {get_psql_binary_path} -c 'CREATE EXTENSION IF NOT EXISTS plperl;'")
+        install_extension = host.run(get_psql_binary_path + " -c \'DROP EXTENSION IF EXISTS plperl CASCADE;'")
+        assert install_extension.rc == 0
+        install_extension = host.run(get_psql_binary_path + " -c \'CREATE EXTENSION IF NOT EXISTS plperl;'")
         assert install_extension.rc == 0
         create_function = """CREATE FUNCTION perl_max (integer, integer) RETURNS integer AS $$
     if ($_[0] > $_[1]) { return $_[0]; }
@@ -48,7 +54,9 @@ def python3_function(host,get_psql_binary_path):
     if os.lower() in ["redhat", "centos", "rhel", "ol"]:
         pytest.skip("Skipping python3 extensions for Centos or RHEL")
     with host.sudo("postgres"):
-        install_extension = host.run(f"PATH=/opt/percona-python3/bin:$PATH {get_psql_binary_path} -c 'CREATE EXTENSION IF NOT EXISTS plpython3u;'")
+        install_extension = host.run(get_psql_binary_path + " -c 'DROP EXTENSION IF EXISTS plpython3u CASCADE;'")
+        assert install_extension.rc == 0
+        install_extension = host.run(get_psql_binary_path + " -c 'CREATE EXTENSION IF NOT EXISTS plpython3u;'")
         assert install_extension.rc == 0
         create_function = """CREATE FUNCTION pymax3 (a integer, b integer)
                   RETURNS integer
@@ -58,7 +66,7 @@ def python3_function(host,get_psql_binary_path):
                   return b
                 $$ LANGUAGE plpython3u;
                         """
-        execute_psql = host.run(get_psql_binary_path+ " -c \'{}\'".format(create_function))
+        execute_psql = host.run(get_psql_binary_path + " -c \'{}\'".format(create_function))
         assert execute_psql.rc == 0, execute_psql.stderr
         assert execute_psql.stdout.strip("\n") == "CREATE FUNCTION", execute_psql.stdout
         return execute_psql
@@ -67,7 +75,9 @@ def python3_function(host,get_psql_binary_path):
 @pytest.fixture()
 def tcl_function(host,get_psql_binary_path):
     with host.sudo("postgres"):
-        install_extension = host.run(f"PATH=/opt/percona-tcl/bin:$PATH {get_psql_binary_path} -c 'CREATE EXTENSION IF NOT EXISTS pltcl;'")
+        install_extension = host.run(get_psql_binary_path + " -c 'DROP EXTENSION IF EXISTS pltcl CASCADE;'")
+        assert install_extension.rc == 0
+        install_extension = host.run(get_psql_binary_path + " -c 'CREATE EXTENSION IF NOT EXISTS pltcl;'")
         assert install_extension.rc == 0
         create_function = """CREATE FUNCTION tcl_max(integer, integer) RETURNS integer AS $$
     if {$1 > $2} {return $1}
@@ -89,30 +99,21 @@ def build_libpq_programm(host):
     host.check_output(lib_dir_cmd)
     if os in ["redhat", "centos", "rhel", "ol"]:
         return host.run(
-            "export LIBPQ_DIR={PG_PATH}/  && export LIBRARY_PATH={PG_PATH}/lib/ &&"
-            "gcc -o lib_version /tmp/libpq_command_temp_dir/lib_version.c -I{} -lpq -std=c99".format(
-                settings.MAJOR_VER, settings.MAJOR_VER, pg_include))
+            "export LIBPQ_DIR={}/  && export LIBRARY_PATH={}/lib/ &&"
+            "gcc -o lib_version /tmp/libpq_command_temp_dir/lib_version.c -I{} -lpq -std=c99".format(PG_PATH,PG_PATH,pg_include))
     return host.run(
         "gcc -o lib_version /tmp/libpq_command_temp_dir/lib_version.c -I{} -lpq -std=c99".format(pg_include))
 
-
 @pytest.mark.parametrize("package", PACKAGES)
-def test_deb_package_is_installed(host, package):
-    os = host.system_info.distribution
-    if os.lower() in ["redhat", "centos", "rhel", "ol"]:
+def test_deb_package_is_installed(host, get_server_path, package):
+    os_name = host.system_info.distribution
+    if os_name.lower() in ["redhat", "centos", "rhel", "ol"]:
         pytest.skip("This test only for Debian based platforms")
-    pkg = host.package(package)
-    assert pkg.is_installed
-    assert pkg.version in pg_versions['deb_pkg_ver'],\
-        f"Expected version {pg_versions['deb_pkg_ver']}. Actual version {pkg.version}"
-
-
-def test_build_libpq_programm(host, build_libpq_programm):
-    assert build_libpq_programm.rc == 0
-    libpq_version = host.run("./lib_version ")
-    assert libpq_version.stdout.strip("\n") == pg_versions['libpq'], libpq_version.stdout
-    assert libpq_version.rc == 0
-
+    with host.sudo():
+        package_filename = os.path.join(get_server_path,'lib', package)
+        file = host.file(package_filename)
+        # Assert that the file exists
+        assert file.exists, f"{package} does not exist."
 
 def test_perl_function(host, perl_function,get_psql_binary_path):
     _ = perl_function
