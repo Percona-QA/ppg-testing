a1fca079ccc1062c66131a78c4956c0e48f1fc98
Naeem Akhter <naeem.akhter@percona.com>
2025-08-21 01:12:19 +0500

    Adding python3.12-etcd package test for RHEL 10.

--- ppg/tests/tests_meta_ha/test_meta_ha.py
+++ ppg/tests/tests_meta_ha/test_meta_ha.py
@@ -6,7 +6,7 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-RPM_PACKAGES = ['percona-patroni', 'etcd', 'percona-haproxy', 'python3-etcd']
+RPM_PACKAGES = ['percona-patroni', 'etcd', 'percona-haproxy', 'python3-etcd', 'python3.12-etcd']
 DEB_PACKAGES = ['percona-patroni', 'etcd', 'percona-haproxy', 'etcd-client', 'etcd-server']
 
 
@@ -29,5 +29,9 @@ def test_rpm_package_is_installed(host, package):
         ds = host.system_info.distribution
         if ds in ["debian", "ubuntu"]:
             pytest.skip("This test only for RHEL based platforms")
+        if package == 'python3-etcd' and host.system_info.release == '10':
+            pytest.skip("This test only for RHEL based version 8 & 9")
+        if package == 'python3.12-etcd' and host.system_info.release in ['8', '9']:
+            pytest.skip("This test only for RHEL based version 10")
         pkg = host.package(package)
         assert pkg.is_installed
