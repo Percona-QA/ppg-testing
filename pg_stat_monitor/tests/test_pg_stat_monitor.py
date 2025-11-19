9ba15719c6d11959529e4316919542fa0b35052b
Artem Gavrilov <artem.gavrilov@percona.com>
2025-11-19 22:38:52 +0200

    Debug

--- pg_stat_monitor/tests/test_pg_stat_monitor.py
+++ pg_stat_monitor/tests/test_pg_stat_monitor.py
@@ -12,9 +12,9 @@ def test_pg_stat_monitor(host):
     with host.sudo("postgres"):
         os = host.system_info.distribution
         if os.lower() in ["redhat", "centos", "rocky", "rhel"] and host.system_info.release == "7":
-            result = host.run("cd /tmp/pg_stat_monitor && export LANG=C && export LC_CTYPE=C && export LC_ALL=C && export PG_TEST_PORT_DIR=tmp/pg_stat_monitor && make installcheck USE_PGXS=1")
+            result = host.run("cd /tmp/pg_stat_monitor && export PG_TEST_NOCLEAN=1 && export LANG=C && export LC_CTYPE=C && export LC_ALL=C && export PG_TEST_PORT_DIR=tmp/pg_stat_monitor && make installcheck USE_PGXS=1")
         else:
-            result = host.run("cd /tmp/pg_stat_monitor && export LANG=C.UTF-8 && export LC_CTYPE=C && export LC_ALL=C && export PG_TEST_PORT_DIR=tmp/pg_stat_monitor && make installcheck USE_PGXS=1")
+            result = host.run("cd /tmp/pg_stat_monitor && export PG_TEST_NOCLEAN=1 && export LANG=C.UTF-8 && export LC_CTYPE=C && export LC_ALL=C && export PG_TEST_PORT_DIR=tmp/pg_stat_monitor && make installcheck USE_PGXS=1")
         print(result.stderr)
         print(result.stdout)
 
@@ -26,13 +26,14 @@ def test_pg_stat_monitor(host):
         print("Contents of /tmp/pg_stat_monitor/tmp_check:")
         print(tmp_check_result.stdout)
 
-        tmp_result_logs = host.run("ls -l /tmp/pg_stat_monitor/tmp_check/log")
-        print("Contents of /tmp/pg_stat_monitor/tmp_check/log:")
-        print(tmp_result_logs.stdout)
+        # tmp_result_logs = host.run("ls -l /tmp/pg_stat_monitor/tmp_check/log")
+        # print("Contents of /tmp/pg_stat_monitor/tmp_check/log:")
+        # print(tmp_result_logs.stdout)
 
-        cat_logs_result = host.run("cat /tmp/pg_stat_monitor/tmp_check/log/*.log")
-        print("Contents of /tmp/pg_stat_monitor/tmp_check/log/.log_file:")
-        print(cat_logs_result.stdout)
+        # cat_logs_result = host.run("cat /tmp/pg_stat_monitor/tmp_check/log/*.log")
+        # print("Contents of /tmp/pg_stat_monitor/tmp_check/log/.log_file:")
+        # print(cat_logs_result.stdout)
+
 
 
         print("-------------------------------- MISC TEST RESULTS --------------------------------")
@@ -49,6 +50,10 @@ def test_pg_stat_monitor(host):
         print("Contents of /tmp/pg_stat_monitor/t/results/021_misc_1.out:")
         print(misc_debug_result.stdout)
 
+        misc_real_log = host.run("cat /tmp/pg_stat_monitor/tmp_check/t_021_misc_1_pgsm_regression_data/pgdata/log/*")
+        print("Contents of /tmp/pg_stat_monitor/tmp_check/t_021_misc_1_pgsm_regression_data/pgdata/log/*:")
+        print(misc_real_log.stdout)
+
         print("-------------------------------- HISTOGRAM TEST RESULTS --------------------------------")
 
         histogram_result_log = host.run("cat /tmp/pg_stat_monitor/tmp_check/log/030_histogram_pgsm_regression.log")
@@ -62,6 +67,10 @@ def test_pg_stat_monitor(host):
         histogram_debug_result = host.run("cat /tmp/pg_stat_monitor/t/results/030_histogram.out.debug")
         print("Contents of /tmp/pg_stat_monitor/t/results/030_histogram.out.debug:")
         print(histogram_debug_result.stdout)
+
+        histogram_real_log = host.run("cat /tmp/pg_stat_monitor/tmp_check/t_030_histogram_pgsm_regression_data/pgdata/log/*")
+        print("Contents of /tmp/pg_stat_monitor/tmp_check/t_030_histogram_pgsm_regression_data/pgdata/log/*:")
+        print(histogram_real_log.stdout)
 
         print("-------------------------------- END --------------------------------")
 
