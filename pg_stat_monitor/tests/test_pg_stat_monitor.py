7934a8a2f5a75826a1266db62a6e706ac56583da
Artem Gavrilov <artem.gavrilov@percona.com>
2025-11-19 17:04:02 +0200

    Debug

--- pg_stat_monitor/tests/test_pg_stat_monitor.py
+++ pg_stat_monitor/tests/test_pg_stat_monitor.py
@@ -17,6 +17,27 @@ def test_pg_stat_monitor(host):
             result = host.run("cd /tmp/pg_stat_monitor && export LANG=C.UTF-8 && export LC_CTYPE=C && export LC_ALL=C && export PG_TEST_PORT_DIR=tmp/pg_stat_monitor && make installcheck USE_PGXS=1")
         print(result.stderr)
         print(result.stdout)
+
+        files_result = host.run("ls -l /tmp/pg_stat_monitor")
+        print("Contents of /tmp/pg_stat_monitor:")
+        print(files_result.stdout)
+
+        logs_result = host.run("ls -l /tmp/pg_stat_monitor/logs")
+        print("Contents of /tmp/pg_stat_monitor/logs:")
+        print(logs_result.stdout)
+
+        tmp_check_result = host.run("ls -l /tmp/pg_stat_monitor/tmp_check")
+        print("Contents of /tmp/pg_stat_monitor/tmp_check:")
+        print(tmp_check_result.stdout)
+
+        tmp_result_logs = host.run("ls -l /tmp/pg_stat_monitor/tmp_check/logs")
+        print("Contents of /tmp/pg_stat_monitor/tmp_check/logs:")
+        print(tmp_result_logs.stdout)
+
+        cat_logs_result = host.run("cat /tmp/pg_stat_monitor/logs/*.log")
+        print("Contents of /tmp/pg_stat_monitor/logs/.log_file:")
+        print(cat_logs_result.stdout)
+
         if result.rc != 0:
             print(host.file("/tmp/pg_stat_monitor/regression.diffs").content_string)
             raise AssertionError
